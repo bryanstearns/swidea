cf9e4915490bd249ec51c7a73d073ba537312516
bryanstearns <bryanstearns@a08d2647-fe4d-0410-8a43-3736cfa3403e>
2008-05-24 21:57:03 +0000

    more cleanup

--- trunk/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    # Enable the admin interface
-    (r'^admin/', include('django.contrib.admin.urls')),
-
-    # Delegate everything else
-    (r'', include('swidea.idea.urls')),
-)
