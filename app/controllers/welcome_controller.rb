037113f5b3fd6bc551947c1f7b5fcd950f187f7f
Bryan Stearns <bryanstearns@users.noreply.github.com>
2008-05-25 01:55:27 +0000

    tweaks to layout; use _path instead of _url

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @pages = Page.find(:all)
-    @ideas = Idea.find(:all)
+    @ideas = Idea.find(:all, :order => :updated_at)
   end
 end
