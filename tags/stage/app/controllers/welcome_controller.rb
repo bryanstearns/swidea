b256e917bfae233b6aa372da6fabfa9f1ac89ee7
bryanstearns <bryanstearns@a08d2647-fe4d-0410-8a43-3736cfa3403e>
2008-05-25 06:56:42 +0000

    Updating swidea stage tag from trunk

--- tags/stage/app/controllers/welcome_controller.rb
+++
@@ -1,6 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    @ideas = Idea.find(:all, :order => "created_at desc")
-    @users = User.find(:all, :order => "created_at desc")
-  end
-end
