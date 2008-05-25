7463c2a209d6d9aad697777ba31b2f6e9f642d45
bryanstearns <bryanstearns@a08d2647-fe4d-0410-8a43-3736cfa3403e>
2008-05-25 21:05:38 +0000

    Updating swidea production tag from trunk

---
+++ tags/production/config/routes.rb
@@ -0,0 +1,13 @@
+ActionController::Routing::Routes.draw do |map|
+  map.resources :ideas
+  map.resources :skills
+  map.resources :pages
+
+  map.resources :users
+  map.resource :session, :controller => 'sessions'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+  map.login  '/login', :controller => 'sessions', :action => 'new'
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+
+  map.root :controller => "welcome"
+end
